bebe423c364e5361cafffffb95b1bf10a4102f63
Nathan Chere <git@nathanchere.com.au>
2012-12-09 22:46:50 +1100

    Initial commit

---
+++ nRage.UnitTests/TVRageClientTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace nRage.UnitTests
+{   
+    public class TVRageClientTests
+    {
+        [Fact]
+        public void CanSearchByTitle() { 
+            throw new NotImplementedException();
+        }
+    }
+}
