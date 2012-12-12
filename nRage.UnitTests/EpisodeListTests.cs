fbc969bac9636f98280cabae4a935b5a03a52c95
Nathan Chere <git@nathanchere.com.au>
2012-12-12 14:08:38 +1100

    Studs for remaining public method tests

---
+++ nRage.UnitTests/EpisodeListTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using Xunit;
+
+namespace nRage.Tests.Unit
+{
+
+    public class EpisodeListTests : TestBase
+    {                
+        private const int showId = 0;
+        private TVRageClient client;
+
+        public EpisodeListTests()
+        {
+            client = _ioc.Get<TVRageClient>();
+        }
+
+        [Fact]
+        public void CanGetEpisodeList()
+        { 
+            var response = client.GetEpisodeList(showId);
+            Assert.True(response != null);
+        }
+
+    }
+}
---
+++ nRage.UnitTests/ShowListTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using Xunit;
+
+namespace nRage.Tests.Unit
+{
+
+    public class ShowListTests : TestBase
+    {                
+        private const int showId = 0;
+        private TVRageClient client;
+
+        public ShowListTests()
+        {
+            client = _ioc.Get<TVRageClient>();
+        }
+
+        [Fact]
+        public void CanGetEpisodeList()
+        { 
+            var response = client.GetShowList();
+            Assert.True(response != null);
+        }
+
+    }
+}
