994ff46ec1d0a85f729302886eb167b6971cb4ad
Nathan Chere <git@nathanchere.com.au>
2012-12-16 22:56:48 +1100

    LastUpdates tests added

---
+++ nRage.UnitTests/LastUpdatesTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using Xunit;
+
+namespace nRage.Tests.Unit
+{
+
+    public class LastUpdatesTests : TVRageClientTestBase
+    {        
+        [Fact]
+        public void CanGetLastUpdatesByTimeSpanInHours()
+        { 
+            var timeSpan = new TimeSpan(0,12,0,0);
+            var response = client.LastUpdates(timeSpan);
+            Assert.True(response != null);
+        }
+    }
+}
