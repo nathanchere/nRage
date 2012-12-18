e339eea425cfdf1e540a1e087a52974b85827075
Nathan Chere <git@nathanchere.com.au>
2012-12-18 15:35:50 +1100

    Refactor metadata service clients to use common base

---
+++ nRage/Clients/ClientBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Linq;
+
+namespace nRage.Clients
+{
+    public abstract class ClientBase
+    {
+        protected IRetriever Retriever {get; set;}
+        
+        public string ApiRoot{get; protected set;}
+
+        protected string GetURL(string format, params object[] args)
+        {
+            var apiMethod = string.Format(format,args);
+            return string.Format(@"{0}/{1}",ApiRoot,apiMethod);
+        }
+
+        protected XDocument GetXML(string path)
+        {
+            return XDocument.Load(Retriever.Get(path));
+        }
+
+    }
+}
