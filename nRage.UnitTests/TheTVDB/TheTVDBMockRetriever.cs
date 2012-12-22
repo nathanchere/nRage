38f9a7c000ec992efbdc3ef75870a5d1e1a13552
Nathan Chere <git@nathanchere.com.au>
2012-12-22 12:45:54 +1100

    Fix Mirrors tests

--- nRage.UnitTests/TheTVDB/TheTVDBMockRetriever.cs
+++ nRage.UnitTests/TheTVDB/TheTVDBMockRetriever.cs
@@ -6,11 +6,11 @@ namespace nRage.Tests.Unit.TheTVDB
     {
         #region URL constants
         public static readonly string MIRRORS = @"mirrors.xml";
-        public static readonly string SERVERTIME = @"mirrors.xml";
-        public static readonly string EPISODELIST = @"mirrors.xml";
-        public static readonly string SEARCH = @"mirrors.xml";
-        public static readonly string SERIESINFO = @"mirrors.xml";
-        public static readonly string UPDATES = @"mirrors.xml";
+        public static readonly string SERVERTIME = @"sexml";
+        public static readonly string EPISODELIST = @"mxml";
+        public static readonly string SEARCH = @"mir.xml";
+        public static readonly string SERIESINFO = @"ml";
+        public static readonly string UPDATES = @"mirrxml";
         #endregion
 
         public TheTVDBMockRetriever() : base(){
@@ -20,6 +20,11 @@ namespace nRage.Tests.Unit.TheTVDB
         protected override void FillCache()
         { 
             Cache(MIRRORS, Properties.TheTVDB.RESPONSE_MIRRORS);
+            Cache(SERVERTIME, Properties.TheTVDB.RESPONSE_UPDATES_NONE);
+            Cache(EPISODELIST, Properties.TheTVDB.RESPONSE_EPISODELIST_81189);
+            //Cache(SEARCH, Properties.TheTVDB.RESPONSE_SEARCH);
+            Cache(SERIESINFO, Properties.TheTVDB.RESPONSE_SERIESINFO_81189);
+            Cache(UPDATES, Properties.TheTVDB.RESPONSE_UPDATES_ALL_123412341);
         }
      
     }
