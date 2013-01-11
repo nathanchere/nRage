acf5ab2dd9b97d329e9a71b50618546eff02eca5
Nathan Chere <git@nathanchere.com.au>
2013-01-11 11:17:07 +1100

    Add common class for API keys

---
+++ nRage/Common.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nRage
+{
+    public static class Common
+    {
+        public static readonly string APIKEY_ROTTENTOMATOES = @"6vy7wgwd6z7n82rp6zyvmutv";
+        public static readonly string APIKEY_THETVDB = @"2A7162D6C1E477B0";        
+    }
+}
