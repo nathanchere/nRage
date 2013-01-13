9513fd096d10342055ee7515531c2c53d82cb1c5
Nathan Chere <git@nathanchere.com.au>
2013-01-14 09:17:54 +1100

    GetEpisodeList fix test series IDs

--- nRage.UnitTests/TheTVDB/EpisodeListTests.cs
+++ nRage.UnitTests/TheTVDB/EpisodeListTests.cs
@@ -11,25 +11,36 @@ namespace nRage.Tests.Unit.TheTVDB
     public class EpisodeListTests : TestBase
     {      
         public EpisodeListTests() : base(){}
-          
+
+        private const int seriesId = 81189;
+        private const int invalidSeriesId = 1;
+
         [Fact]
         public void CanGetEpisodeList()
-        { 
-            var response = client.GetEpisodeList(1);
+        {
+            var response = client.GetEpisodeList(seriesId);
             Assert.True(response != null);
+        }
+
+        [Fact]
+        public void GetEpisodeListThrowsIfInvalidSeriesId()
+        {
+            Assert.Throws<ShowNotFoundException>(() =>
+                client.GetEpisodeList(invalidSeriesId)
+            );
         }
 
         [Fact]
         public void GetEpisodeListCallsCorrectURL()
         { 
-            client.GetEpisodeList(1);
-            Assert.True(MockRetriever.GetLastURLCalled() == TheTVDBMockRetriever.EPISODELIST);
+            client.GetEpisodeList(seriesId);
+            Assert.True(MockRetriever.GetLastURLCalled() == TheTVDBMockRetriever.EPISODELIST_81189);
         }
 
         [Fact]
         public void GetEpisodeListReturnsCorrectX()
         {
-            var response = client.GetEpisodeList(1);
+            var response = client.GetEpisodeList(seriesId);
             Assert.False(true);
         }
 
