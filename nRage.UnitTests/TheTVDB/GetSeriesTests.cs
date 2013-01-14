e11ed056609140c58d889138eea675ff41d42e3b
Nathan Chere <git@nathanchere.com.au>
2013-01-14 17:06:46 +1100

    GetSeries logic finished, all tests passing

--- nRage.UnitTests/TheTVDB/GetSeriesTests.cs
+++ nRage.UnitTests/TheTVDB/GetSeriesTests.cs
@@ -30,10 +30,60 @@ namespace nRage.Tests.Unit.TheTVDB
         }
 
         [Fact]
-        public void GetSeriesReturnsCorrectX()
+        public void GetSeriesReturnsCorrectSeriesList()
         {
-            var response = client.GetSeries("");
-            Assert.False(true);
+            var response = client.GetSeries(query);
+            Assert.True(response.Series != null);
+            Assert.True(response.Series.Count == 3);
+        }
+
+        [Fact]
+        public void GetSeriesReturnsCorrectId()
+        {
+            var response = client.GetSeries(query);
+            Assert.True(response.Series[0].Id  == "73244");
+        }
+
+        [Fact]
+        public void GetSeriesReturnsCorrectSeriesId()
+        {
+            var response = client.GetSeries(query);
+            Assert.True(response.Series[0].SeriesId == "73244");
+        }
+
+        [Fact]
+        public void GetSeriesReturnsCorrectLanguage()
+        {
+            var response = client.GetSeries(query);
+            Assert.True(response.Series[0].Language == "en");
+        }
+
+        [Fact]
+        public void GetSeriesReturnsCorrectSeriesName()
+        {
+            var response = client.GetSeries(query);
+            Assert.True(response.Series[0].SeriesName == "The Office (US)");
+        }
+
+        [Fact]
+        public void GetSeriesReturnsCorrectBanner()
+        {
+            var response = client.GetSeries(query);
+            Assert.True(response.Series[0].Banner == "graphical/73244-g9.jpg");
+        }
+
+        [Fact]
+        public void GetSeriesReturnsCorrectOverview()
+        {
+            var response = client.GetSeries(query);
+            Assert.True(response.Series[0].Overview == "A fresh and funny mockumentary-style glimpse into the daily interactions of the eccentric workers at the Dunder Mifflin paper supply company. This fast-paced comedy parodies contemporary American water-cooler culture.");
+        }
+
+        [Fact]
+        public void GetSeriesReturnsCorrectFirstAired()
+        {
+            var response = client.GetSeries(query);
+            Assert.True(response.Series[0].FirstAired == "2005-03-24");
         }
 
     }
