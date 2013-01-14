dd921197380fd5c8ef6bc068ec6cc0107c2298b6
Nathan Chere <git@nathanchere.com.au>
2013-01-15 09:09:54 +1100

    Refactor TvrageClient mapper

---
+++ nRage/Clients/ITvrageMapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using nRage.Contract.TVRage;
+
+namespace nRage.Clients
+{
+    public interface ITvrageMapper
+    {
+        ShowInfoResponse MapXMLToShowInfoResponse(XDocument xml);
+        EpisodeListResponse MapXMLToEpisodeListResponse(XDocument xml);
+        EpisodeInfoResponse MapXMLToEpisodeInfoResponse(XDocument xml);
+        FullShowInfoResponse MapXMLToFullShowInfoResponse(XDocument xml);
+        List<ShowListResult> MapXMLToShowListResponse(XDocument xml);
+        List<FullSearchResult> MapXMLToFullSearchResults(XDocument xml);
+        List<SearchResult> MapXMLToSearchResponse(XDocument xml);
+        LastUpdatesResponse MapXMLToEpisodeLastUpdates(XDocument xml);
+    }
+}
--- nRage/Clients/TVRageClient.cs
+++ nRage/Clients/TVRageClient.cs
@@ -12,10 +12,10 @@ using nRage.Contract.TVRage;
 
 namespace nRage.Clients {
 
-    public class TVRageClient : ClientBase
+    public class TvrageClient : ClientBase
     {
-        public TVRageClient():base(){}
-        public TVRageClient(IRetriever retriever) : base(retriever) {}        
+        private readonly TvrageMapper _mapper;
+        public TvrageClient(IRetriever retriever) : base(retriever) { _mapper = new TvrageMapper(); }               
 
         #region URL generation
         public override string ApiRoot { get { return @"http://services.tvrage.com/feeds"; } }
@@ -60,7 +60,7 @@ namespace nRage.Clients {
         { 
             var response = GetXML(GetURLForLastUpdates(hours));
             
-            return MapXMLToEpisodeLastUpdates(response);
+            return _mapper.MapXMLToEpisodeLastUpdates(response);
         }        
 
         public SearchResponse SearchByTitle(string title) {
@@ -72,7 +72,7 @@ namespace nRage.Clients {
             if (response.Root == null || response.Root.Value == "0")
                 return result;
 
-            result.Results = MapXMLToSearchResponse(response);
+            result.Results = _mapper.MapXMLToSearchResponse(response);
             return result;
         }
 
@@ -85,7 +85,7 @@ namespace nRage.Clients {
             if (response.Root == null || response.Root.Value == "0")
                 return result;
 
-            result.Results = MapXMLToFullSearchResults(response);
+            result.Results = _mapper.MapXMLToFullSearchResults(response);
             return result;
         }
 
@@ -95,13 +95,13 @@ namespace nRage.Clients {
             if (response.Root == null || response.Root.Value == "")
                 throw new ShowNotFoundException();
 
-            return MapXMLToShowInfoResponse(response);
+            return _mapper.MapXMLToShowInfoResponse(response);
         }
 
         public EpisodeListResponse GetEpisodeList(int showId) {
             var response = GetXML(GetURLForEpisodeList(showId));
             
-            return MapXMLToEpisodeListResponse(response);
+            return _mapper.MapXMLToEpisodeListResponse(response);
         }
 
         public EpisodeInfoResponse GetEpisodeInfo(int showID, int season, int episode) {
@@ -114,183 +114,24 @@ namespace nRage.Clients {
             ValidateResponse(rawResponse);    
         
             var response = XDocument.Load(rawResponse);
-            return MapXMLToEpisodeInfoResponse(response);
+            return _mapper.MapXMLToEpisodeInfoResponse(response);
         }
-
 
         public FullShowInfoResponse GetFullShowInfo(int showId) {            
             var rawResponse = Retriever.Get(GetURLForFullShowInfo(showId));
             ValidateResponse(rawResponse);            
 
             var response = XDocument.Load(rawResponse);
-            return MapXMLToFullShowInfoResponse(response);
+            return _mapper.MapXMLToFullShowInfoResponse(response);
         }
 
         public ShowListResponse GetShowList() {
             var response = GetXML(GetURLForShowList());
             var result = new ShowListResponse();
-            result.Results = MapXMLToShowListResponse(response);
+            result.Results = _mapper.MapXMLToShowListResponse(response);
             return result;
         }
-        #endregion
-
-        #region OXM (Object-XML Mapper) - because the software world needs more acronyms
-        private ShowInfoResponse MapXMLToShowInfoResponse(XDocument xml) {
-            return xml.Descendants("Showinfo").Select(x => new ShowInfoResponse {
-                ShowID = (int)x.Element("showid"),
-                ShowName = (string)x.Element("showname"),
-                ShowLink = (string)x.Element("showlink"),
-                Seasons = (string)x.Element("seasons"),
-                Started = (string)x.Element("started"),
-                StartDate = (string)x.Element("startdate"),
-                Ended = (string)x.Element("ended"),
-                OriginCountry = (string)x.Element("origin_country"),
-                Status = (string)x.Element("status"),
-                Genres = x.Descendants("genre").Select(y => y.Value).ToList(),
-                RunTime = (string)x.Element("runtime"),
-                Network = (string)x.Element("network"),
-                AirDay = (string)x.Element("airday"),
-                AirTime = (string)x.Element("airtime"),
-                TimeZone = (string)x.Element("timezone"),
-                Classification = (string)x.Element("classification"),
-            }).Single();
-        }
-
-        private EpisodeListResponse MapXMLToEpisodeListResponse(XDocument xml) {
-            return xml.Descendants("Show").Select(x => new EpisodeListResponse {
-                Name = (string)x.Element("name"),
-                TotalSeasons = (string)x.Element("totalseasons"),
-                Seasons = x.Descendants("EpisodeListResultSeason").Select(y=>new EpisodeListResultSeason{
-                    Number = y.Attribute("no").Value.ToString(),
-                    Episodes = y.Descendants("episode").Select(z=>new EpisodeListResultEpisode{
-                        EpNum = (string)z.Element("epnum"),
-                        SeasonNum = (string)z.Element("seasonnum"),
-                        Link = (string)z.Element("link"),
-                        AirDate = (string)z.Element("airdate"),
-                        Title = (string)z.Element("title"),
-                        ProdNum = (string)z.Element("prodnum"), 
-                    }).ToList(),
-                }).ToList(),
-                                 
-            }).Single();
-        }
-
-        private EpisodeInfoResponse MapXMLToEpisodeInfoResponse(XDocument xml) {
-            return xml.Descendants("show").Select(x => new EpisodeInfoResponse {
-                Name = (string)x.Element("name"),
-                Link = (string)x.Element("link"),
-                Started = (string)x.Element("started"),
-                Ended = (string)x.Element("ended"),
-                Country = (string)x.Element("country"),
-                Status = (string)x.Element("status"),
-                Classification = (string)x.Element("classification"),
-                Genres = x.Descendants("genre").Select(y=>y.Value).ToList(),
-                AirTime = (string)x.Element("airtime"),
-                RunTime = (string)x.Element("runtime"),
-                Episode = x.Descendants("episode").Select(y=>new EpisodeResult{
-                    AirDate = (string)y.Element("airdate"),
-                    Title = (string)y.Element("title"),
-                    Number = (string)y.Element("number"),
-                    URL = (string)y.Element("url"),
-                }).SingleOrDefault(),
-            }).Single();
-        }
-
-        private FullShowInfoResponse MapXMLToFullShowInfoResponse(XDocument xml) {
-            return xml.Descendants("Show").Select(x => new FullShowInfoResponse {
-                ShowID = (int)x.Element("showid"),
-                Name = (string)x.Element("name"),
-                ShowLink = (string)x.Element("showlink"),
-                TotalSeasons = (string)x.Element("totalseasons"),
-                Started = (string)x.Element("started"),
-                Ended = (string)x.Element("ended"),
-                Image = (string)x.Element("image"),
-                OriginCountry = (string)x.Element("origin_country"),
-                Status = (string)x.Element("status"),
-                Genres = x.Descendants("genre").Select(y => y.Value).ToList(),
-                RunTime = (string)x.Element("runtime"),
-                Network = (string)x.Element("network"),
-                AirDay = (string)x.Element("airday"),
-                AirTime = (string)x.Element("airtime"),
-                TimeZone = (string)x.Element("timezone"),
-                Classification = (string)x.Element("classification"),
-                Seasons = x.Descendants("EpisodeListResultSeason").Select(y=>new EpisodeListResultSeason{
-                    Number = y.Attribute("no").Value.ToString(),
-                    Episodes = y.Descendants("episode").Select(z=>new EpisodeListResultEpisode{
-                        EpNum = (string)z.Element("epnum"),
-                        SeasonNum = (string)z.Element("seasonnum"),
-                        Link = (string)z.Element("link"),
-                        AirDate = (string)z.Element("airdate"),
-                        Title = (string)z.Element("title"),
-                        ProdNum = (string)z.Element("prodnum"), 
-                    }).ToList(),
-                }).ToList(),
-            }).Single();
-        }
-
-        private List<ShowListResult> MapXMLToShowListResponse(XDocument xml) {
-            return xml.Descendants("show").Select(x => new ShowListResult {
-                ID = (int)x.Element("id"),
-                Name = (string)x.Element("name"),
-                Country = (string)x.Element("country"),
-                Status = (string)x.Element("status"),
-            }).ToList();
-        }
-
-        private List<FullSearchResult> MapXMLToFullSearchResults(XDocument xml) {
-            return xml.Descendants("show").Select(x => new FullSearchResult {
-                ShowID = (int)x.Element("showid"),
-                Name = (string)x.Element("name"),
-                Link = (string)x.Element("link"),
-                Country = (string)x.Element("country"),
-                Started = (string)x.Element("started"),
-                Ended = (string)x.Element("ended"),
-                Seasons = (string)x.Element("seasons"),
-                Status = (string)x.Element("status"),
-                Classification = (string)x.Element("classification"),
-                Genres = x.Descendants("genre").Select(y => y.Value).ToList(),
-                RunTime = (string)x.Element("runtime"),
-                AirTime = (string)x.Element("airtime"),
-                Network = (string)x.Element("network"),
-                AirDay = (string)x.Element("airday"),
-                AKAs = x.Descendants("aka").Select(y => new AKA {
-                    Country = (string)y.Attribute("country"),
-                    Name = y.Value,
-                }).ToList(),
-            }).ToList();
-        }
-
-        private List<SearchResult> MapXMLToSearchResponse(XDocument xml) {
-            return xml.Descendants("show").Select(x => new SearchResult {
-                ShowID = (int)x.Element("showid"),
-                Name = (string)x.Element("name"),
-                Link = (string)x.Element("link"),
-                Country = (string)x.Element("country"),
-                Started = (string)x.Element("started"),
-                Ended = (string)x.Element("ended"),
-                Seasons = (string)x.Element("seasons"),
-                Status = (string)x.Element("status"),
-                Classification = (string)x.Element("classification"),
-                Genres = x.Descendants("genre").Select(y => y.Value).ToList(),
-            }).ToList();
-        }
-
-        private LastUpdatesResponse MapXMLToEpisodeLastUpdates(XDocument xml) { 
-            return new LastUpdatesResponse{
-                Timestamp = (string)xml.Element("updates").Attribute("at"),
-                UpdatesCount = (string)xml.Element("updates").Attribute("found"),
-                Sorting = (string)xml.Element("updates").Attribute("sorting"),
-                ShowingPeriod = (string)xml.Element("updates").Attribute("showing"),
-
-                Shows = xml.Descendants("show").Select(x=>new LastUpdatesShowResult{
-                    ShowID = (int)x.Element("id"),
-                    Last = (string)x.Element("last"),
-                    LatestEpisode = (string)x.Element("lastepisode"),
-                }).ToList(),
-            };
-
-        }
-        #endregion
+        #endregion   
         
         private void ValidateResponse(Stream rawResponse)
         {
