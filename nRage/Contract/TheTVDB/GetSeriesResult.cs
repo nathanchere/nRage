0755d76745ec00c7d47964dfd73e191d60917c7a
Nathan Chere <git@nathanchere.com.au>
2013-01-14 16:39:03 +1100

    GetSeries contracts

---
+++ nRage/Contract/TheTVDB/GetSeriesResult.cs
@@ -0,0 +1,13 @@
+﻿namespace nRage.Contract.TheTVDB
+{
+    public class GetSeriesResult
+    {
+        public string Id { get; set; }
+        public string SeriesId { get; set; }
+        public string Language { get; set; }
+        public string SeriesName { get; set; }
+        public string Banner { get; set; }
+        public string Overview { get; set; }
+        public string FirstAired { get; set; }
+    }
+}
