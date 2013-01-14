48180be1daf05e5ee957eeda1569cdbfa515c186
Nathan Chere <git@nathanchere.com.au>
2013-01-14 14:43:37 +1100

    Change Search contracts to GetSeries

--- nRage/Clients/ITvdbResponseMapper.cs
+++ nRage/Clients/ITvdbResponseMapper.cs
@@ -6,12 +6,12 @@ namespace nRage.Clients
 {
     public interface ITvdbResponseMapper
     {
-        List<string> ConvertPipedStringToList(string input);
         MirrorsResponse MapXMLToMirrors(XDocument xml);
         ServerTimeResponse MapXMLToServerTime(XDocument xml);
         GetUpdatesResponse MapXMLToUpdates(XDocument xml);
         SeriesInfoResponse MapXMLToSeriesInfo(XDocument xml);
-        SearchResponse MapXMLToSearch(XDocument xml);
+        GetSeriesResponse MapXMLToGetSeries(XDocument xml);
         EpisodeListResponse MapXMLToEpisodeList(XDocument xml);
+        GetSeriesResponse MapXMLToGetSeriesById(XDocument xml);
     }
 }
