af4e8b00b1366552af22bf6c1485ace693e03a3d
Nathan Chere <git@nathanchere.com.au>
2013-01-14 14:45:03 +1100

    Add GetSeries test shell

---
+++ nRage.UnitTests/TheTVDB/GetSeriesTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using Xunit;
+
+namespace nRage.Tests.Unit.TheTVDB
+{
+    public class GetSeriesTests : TestBase
+    {      
+        public GetSeriesTests() : base(){}
+          
+        [Fact]
+        public void CanGetSeries()
+        { 
+            var response = client.GetSeries("");
+            Assert.True(response != null);
+        }
+
+        [Fact]
+        public void GetSeriesCallsCorrectURL()
+        { 
+            client.GetSeries("");
+            Assert.True(MockRetriever.GetLastURLCalled() == TheTVDBMockRetriever.GETSERIES);
+        }
+
+        [Fact]
+        public void GetSeriesReturnsCorrectX()
+        {
+            var response = client.GetSeries("");
+            Assert.False(true);
+        }
+
+    }
+}
---
+++ nRage/Contract/TheTVDB/GetSeriesResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nRage.Contract.TheTVDB
+{
+    public class GetSeriesResponse
+    {
+    }
+
+    public class GetSeriesByIdResponse
+    {
+    }
+}
